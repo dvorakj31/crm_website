3a230ede729be5bef10c1144c15a0c74248241e4
Jakub Dvorak <dvorakj31@gmail.com>
2018-08-01 13:35:57 +0200

    settings + warning emails

--- crm_api/views.py
+++ crm_api/views.py
@@ -36,7 +36,60 @@ def download_file(request, file_id):
     response = FileResponse(cust_file.files, content_type = 'file/%s' % mime[0].split('/')[1])
     response['Content-Disposition'] = "attachment;filename=%s" % file_name.split('/')[1]
     return response
+
+
+@login_required
+def select_customer(request):
+    return render(request, 'crm_api/html/_search.html')
+
+
+@login_required
+def settings_view(request):
+    return render(request, 'crm_api/html/settings.html')
+
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = CustomPasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            # messages.success(request, 'Heslo uspesne zmeneno')
+            return redirect('/crm')
+        else:
+            pass  # messages.error(request, 'Prosim, opravte chyby nize')
+    else:
+        form = CustomPasswordChangeForm(request.user)
+    return render(request, 'crm_api/html/change_password.html', {
+        'form': form
+    })
+
+
+@login_required
+@permission_required('crm_api.add_warningemail')
+def set_emails(request):
+    if request.method == 'POST':
+        val = request.POST.get('warningemail_id')
+        return HttpResponseRedirect(reverse('crm_api:edit_email', kwargs={'pk': val}))
+    return render(request, 'crm_api/html/set_emails.html', {'object_list': WarningEmail.objects.values()})
     
+# Customer views starts here
+class CustomerSearchList(LoginRequiredMixin, generic.ListView):
+    model = Customer
+    template_name = 'crm_api/html/customer_search_result.html'
+    login_url = '/crm/login/'
+    paginate_by = PAGE_NUM
+
+    def get_queryset(self):
+        query_set = Customer.objects.all()
+        search_query = self.request.GET.get('q', None)
+        search_filter = self.request.GET.get('filter_papers', None)
+        query_set = Customer.objects.filter(name__contains='%s' % search_query)
+        if search_filter != 'all' and search_filter in ['yes', 'no']:
+            query_set = query_set.filter(papers__exact=search_filter == 'yes')
+        return query_set
+
 
 class CustomerList(LoginRequiredMixin, generic.ListView):
     model = Customer
@@ -71,6 +124,7 @@ class CustomerDelete(PermissionRequiredMixin, LoginRequiredMixin, generic.Delete
     login_url = '/crm/login/'
 
 
+# Customer file views starts here
 class CustomerFilesCreateView(LoginRequiredMixin, generic.CreateView):
     model = CustomerFiles
     success_url = '/crm/'
@@ -115,4 +169,47 @@ class CustomerFilesDelete(PermissionRequiredMixin, LoginRequiredMixin, generic.D
     model = CustomerFiles
     success_url = reverse_lazy('crm_api:index')
     fields = ['files']
+    login_url = '/crm/login/'
+
+
+# Settings views starts here
+class CustomPasswordChangeForm(PasswordChangeForm):
+    error_css_class = 'has-error'
+    error_messages = {'password_incorrect': 'Nespravne heslo',
+                      'password_mismatch': 'Hesla se neshoduji'}
+    old_password = forms.CharField(required=True, label='Stare heslo',
+                                   widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+                                   error_messages={'required': 'Vyplnte toto pole'})
+    new_password1 = forms.CharField(required=True, label='Nove heslo',
+                                    widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+                                    error_messages={'required': 'Vyplnte toto pole'})
+    new_password2 = forms.CharField(required=True, label='Potvrdte nove heslo',
+                                    widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+                                    error_messages={'required': 'Vyplnte toto pole'})
+
+
+#Warning email views starts here
+class WarningEmailCreateView(PermissionRequiredMixin, LoginRequiredMixin, generic.CreateView):
+    permission_required = 'crm_api.add_warningemail'
+    model = WarningEmail
+    success_url = '/crm/set_emails'
+    fields = '__all__'
+    template_name = 'crm_api/html/create_email.html'
+    login_url = '/crm/login/'
+
+
+class WarningEmailUpdate(PermissionRequiredMixin, LoginRequiredMixin, generic.UpdateView):
+    permission_required = 'crm_api.change_warningemail'
+    model = WarningEmail
+    template_name = 'crm_api/html/update_email.html'
+    fields = '__all__'
+    success_url = '/crm/set_emails'
+    login_url = '/crm/login/'
+
+
+class WarningEmailDelete(PermissionRequiredMixin, LoginRequiredMixin, generic.DeleteView):
+    permission_required = 'crm_api.delete_warningemail'
+    model = WarningEmail
+    success_url = reverse_lazy('crm_api:index')
+    fields = ['name']
     login_url = '/crm/login/'
