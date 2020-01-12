eba515199556d46ef9e92f503286469af9906f6d
Jakub Dvořák <dvoraj84@fit.cvut.cz>
2020-01-12 14:26:36 +0100

    deadline tag

--- crm_api/templatetags/date_extras.py
+++ crm_api/templatetags/date_extras.py
@@ -24,3 +24,23 @@ def last_month(with_year=False):
 @register.simple_tag
 def get_quartal():
     return str(math.ceil(datetime.datetime.now().month / 3))
+
+
+@register.simple_tag
+def get_vat_deadline(is_monthly):
+    now = datetime.datetime.now()
+    date = "25. "
+    if is_monthly:
+        date += f'{now.month if now.day <= 25 else ((now.month + 1) % 13) + 1}.'
+    else:
+        month = 1
+        if 1 <= now.month <= 4:
+            month = 4
+        elif 5 <= now.month <= 7:
+            month = 7
+        elif 8 <= now.month <= 10:
+            month = 10
+        if now.day > 25 and month == now.month:
+            month = (month + 3) % 12
+        date += (str(month) + '.')
+    return date
